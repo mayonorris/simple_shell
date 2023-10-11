53da5e4dd5b1e7310665ead0b6b3801fa60dffbb
rigo1999 <letlovelead271@gmail.com>
2023-10-11 16:14:25 +0200

    main and shell lib" "

---
+++ main_shell.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+/**
+ * main_shell - The main function of our shell
+ * @ac: counts the number of arguments
+ * @av: arguments array
+ * Return: 0 Always for success case
+*/
+int main(int ac, char **argv)
+{
+
+
+
+}
---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
+
+
+
+#endif
